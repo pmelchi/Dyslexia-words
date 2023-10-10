a01316ce952f29d43e0ab211bf7f3be0e7eae535
Pablo Melchi <pmelchi@users.noreply.github.com>
2023-10-10 07:27:46 -0500

    Version working with map of words

--- textapi/routes/textProcessor.js
+++ textapi/routes/textProcessor.js
@@ -1,18 +1,39 @@
 function processText(text) {
+  //Tokenize the text by spaces
+  let tokens = text.split(' ');
+  
+  //Load the map
+  const map = loadMap();
+  
+  //Iterate through the tokens and add the text with an underline if it is in the map
+  for (let i = 0; i < tokens.length; i++) {
+    console.log(tokens[i].toLowerCase());
+
+
+    if (map.has(tokens[i].toLowerCase())) {
+
+      //Add the text with underline and a tooltip
+      tokens[i] = '<span style="text-decoration: underline; text-decoration-color: blue;" title="' + map.get(tokens[i].toLowerCase()) + '">' + tokens[i] + '</span>';
+    } else {
+
+    }
+  }
+  
   // Highlight words ending with 'ing'
-  text = text.replace(/\b(\w*ing)\b/g, '<span style="background-color: yellow;">$1</span>');
+ //text = text.replace(/\b(\w*ing)\b/g, '<span style="background-color: yellow;">$1</span>');
 
   // Underline words ending with 'ed'
-  text = text.replace(/\b(\w*ed)\b/g, '<span style="text-decoration: underline; text-decoration-color: blue;">$1</span>');
+  //text = text.replace(/\b(\w*ed)\b/g, '<span style="text-decoration: underline; text-decoration-color: blue;">$1</span>');
 
-  return text;
+  //Return the processed text
+  return tokens.join(' ');
 }
 
 //Write a function that loads a json file that contains key/pair combinations and load it to a map
 function loadMap() {
   const fs = require('fs');
   const map = new Map();
-  const data = fs.readFileSync('./data.json', 'utf8');
+  const data = fs.readFileSync('./db/words.json', 'utf8');
   const json = JSON.parse(data);
   for (let key in json) {
     map.set(key, json[key]);
